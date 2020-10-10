5c2cd8ffe2fbc2e2ddc701d53d87c11b14a1a191
eddie <aigagror@gmail.com>
2020-10-10 17:50:19 -0500

    decreased default learning rates to 1e-2

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ parser.add_argument('--samples', type=int, default=1024)
 # Transfer
 parser.add_argument('--steps', type=int, default=500, help='num training steps')
 parser.add_argument('--imsize', type=int, default=224, help='image size')
-parser.add_argument('--img-lr', type=float, default=2e-2,
+parser.add_argument('--img-lr', type=float, default=1e-2,
                     help='learning rate for image pixels')
-parser.add_argument('--disc-lr', type=float, default=2e-2,
+parser.add_argument('--disc-lr', type=float, default=1e-2,
                     help='learning rate for discriminators')
 parser.add_argument('--alpha', type=float, default=0.2, help='alpha')
 parser.add_argument('--device', choices=['cuda', 'cpu'], default='cpu')
