87441533cab78f93b5dc1c84938119f34213bed5
eddie <aigagror@gmail.com>
2020-11-12 13:29:58 -0600

    dynamic display

--- style/__init__.py
+++ style/__init__.py
@@ -70,7 +70,7 @@ def transfer(args, gen_img, style_img, model):
 
             # Display image?
             if args.display is not None and i % args.display == 0:
-                gen_hist.append(F.to_pil_image(gen_img))
+                gen_hist.append(F.to_pil_image(gen_img.squeeze(0)))
                 display_torch_img(gen_img, out)
 
     except KeyboardInterrupt:
