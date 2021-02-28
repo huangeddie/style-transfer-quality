ca52962c6ff81c1ca89f294f21a210cf0c457dbb
eddie <aigagror@gmail.com>
2021-02-28 14:44:12 -0600

    reimplemented skewness

--- discriminators.py
+++ discriminators.py
@@ -30,11 +30,8 @@ class ThirdMomentLoss(tf.keras.losses.Loss):
         std1 = tf.math.reduce_std(y_true, axis=1, keepdims=True)
         std2 = tf.math.reduce_std(y_pred, axis=1, keepdims=True)
 
-        z1 = (y_true - mu1) / (std1 + 1e-3)
-        z2 = (y_true - mu2) / (std2 + 1e-3)
-
-        skew1 = (tf.reduce_mean(z1 ** 3))
-        skew2 = (tf.reduce_mean(z2 ** 3))
+        skew1 = tf.reduce_mean((y_true - mu1) ** 3, keepdims=True) / (std1 + 1e-3) ** 3
+        skew2 = tf.reduce_mean((y_true - mu2) ** 3, keepdims=True) / (std2 + 1e-3) ** 3
 
         loss = (mu1 - mu2) ** 2 + (std1 - std2) ** 2 + (skew1 - skew2) ** 2
 
