bc7f6185df38952444c9b4745b3bd18377c6721a
eddie <aigagror@gmail.com>
2021-02-28 14:54:23 -0600

    bug fix

--- discriminators.py
+++ discriminators.py
@@ -21,19 +21,23 @@ class FirstMomentLoss(tf.keras.losses.Loss):
 
 class ThirdMomentLoss(tf.keras.losses.Loss):
     def call(self, y_true, y_pred):
-        tf.debugging.assert_rank(y_true, 3)
-        tf.debugging.assert_rank(y_pred, 3)
+        feats1, feats2 = y_true, y_pred
+        tf.debugging.assert_rank(feats1, 3)
+        tf.debugging.assert_rank(feats2, 3)
 
-        mu1 = tf.reduce_mean(y_true, axis=1, keepdims=True)
-        mu2 = tf.reduce_mean(y_pred, axis=1, keepdims=True)
+        mu1 = tf.reduce_mean(feats1, axis=1, keepdims=True)
+        mu2 = tf.reduce_mean(feats2, axis=1, keepdims=True)
 
-        std1 = tf.math.reduce_std(y_true, axis=1, keepdims=True)
-        std2 = tf.math.reduce_std(y_pred, axis=1, keepdims=True)
+        var1 = tf.math.reduce_variance(feats1, axis=1, keepdims=True)
+        var2 = tf.math.reduce_variance(feats2, axis=1, keepdims=True)
 
-        skew1 = tf.reduce_mean((y_true - mu1) ** 3, keepdims=True) / (std1 + 1e-3) ** 3
-        skew2 = tf.reduce_mean((y_true - mu2) ** 3, keepdims=True) / (std2 + 1e-3) ** 3
+        std1 = tf.math.reduce_std(feats1, axis=1, keepdims=True)
+        std2 = tf.math.reduce_std(feats2, axis=1, keepdims=True)
 
-        loss = tf.abs(mu1 - mu2) + tf.abs(std1 - std2) + tf.abs(skew1 - skew2)
+        skew1 = tf.reduce_mean((feats1 - mu1) ** 3, keepdims=True) / (std1 + 1e-5) ** 3
+        skew2 = tf.reduce_mean((feats2 - mu2) ** 3, keepdims=True) / (std2 + 1e-5) ** 3
+
+        loss = tf.abs(mu1 - mu2) + tf.abs(var1 - var2) + tf.abs(skew1 - skew2)
 
         return loss
 
