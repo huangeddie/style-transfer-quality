32620f1f85ff67ab2d7651fdc0a055077d85ff39
eddie <aigagror@gmail.com>
2021-03-05 13:58:15 -0600

    Added covar loss

--- distributions/losses.py
+++ distributions/losses.py
@@ -23,6 +23,38 @@ class SecondMomentLoss(tf.keras.losses.Loss):
 
         return (mu1 - mu2) ** 2 + (var1 - var2) ** 2
 
+class CoVarLoss(tf.keras.losses.Loss):
+    def call(self, y_true, y_pred):
+        feats1, feats2 = reshape_to_feats(y_true, y_pred)
+
+        mu1 = tf.reduce_mean(feats1, axis=1, keepdims=True)
+        mu2 = tf.reduce_mean(feats2, axis=1, keepdims=True)
+
+        mean_loss = tf.reduce_mean((mu1 - mu2) ** 2, axis=1)
+
+        centered_feats1 = feats1 - mu1
+        centered_feats2 = feats2 - mu2
+
+        n = tf.cast(tf.shape(centered_feats1)[1], tf.float32)
+        covar1 = tf.einsum('bnc,bnd->bcd', centered_feats1, centered_feats1) / n
+        covar2 = tf.einsum('bnc,bnd->bcd', centered_feats2, centered_feats2) / n
+
+        covar_loss = tf.reduce_mean((covar1 - covar2) ** 2, axis=[1, 2])
+
+        return mean_loss + covar_loss
+
+
+class GramianLoss(tf.keras.losses.Loss):
+    def call(self, y_true, y_pred):
+        tf.debugging.assert_rank(y_true, 4)
+        imshape = tf.shape(y_true)
+        num_locs = tf.cast(imshape[1] * imshape[2], y_true.dtype)
+
+        gram_true = tf.linalg.einsum('bijc,bijd->bcd', y_true, y_true) / num_locs
+        gram_pred = tf.linalg.einsum('bijc,bijd->bcd', y_pred, y_pred) / num_locs
+
+        return (gram_true - gram_pred) ** 2
+
 
 class ThirdMomentLoss(tf.keras.losses.Loss):
     def call(self, y_true, y_pred):
@@ -38,18 +70,6 @@ class ThirdMomentLoss(tf.keras.losses.Loss):
         skew2 = tf.reduce_mean(z2 ** 3, axis=[1, 2], keepdims=True)
 
         return (mu1 - mu2) ** 2 + (var1 - var2) ** 2 + (skew1 - skew2) ** 2
-
-
-class GramianLoss(tf.keras.losses.Loss):
-    def call(self, y_true, y_pred):
-        tf.debugging.assert_rank(y_true, 4)
-        imshape = tf.shape(y_true)
-        num_locs = tf.cast(imshape[1] * imshape[2], y_true.dtype)
-
-        gram_true = tf.linalg.einsum('bijc,bijd->bcd', y_true, y_true) / num_locs
-        gram_pred = tf.linalg.einsum('bijc,bijd->bcd', y_pred, y_pred) / num_locs
-
-        return (gram_true - gram_pred) ** 2
 
 
 class WassLoss(tf.keras.losses.Loss):
@@ -73,5 +93,5 @@ class CoWassLoss(tf.keras.losses.Loss):
         return tf.reduce_mean(wass_loss, axis=1) + tf.reduce_mean(covar_loss, axis=[1, 2])
 
 
-loss_dict = {'m1': FirstMomentLoss(), 'm2': SecondMomentLoss(), 'gram': GramianLoss(), 'm3': ThirdMomentLoss(),
-             'wass': WassLoss(), 'cowass': CoWassLoss()}
+loss_dict = {'m1': FirstMomentLoss(), 'm2': SecondMomentLoss(), 'covar':CoVarLoss(), 'gram': GramianLoss(),
+             'm3': ThirdMomentLoss(), 'wass': WassLoss(), 'cowass': CoWassLoss()}
