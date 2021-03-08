57c6be5d42f45852822a2016f91f9e0c852ec9bf
eddie <aigagror@gmail.com>
2021-03-07 22:34:13 -0600

    removed single feats from randpairwass

--- distributions/losses.py
+++ distributions/losses.py
@@ -98,10 +98,7 @@ class RandPairWassLoss(tf.keras.losses.Loss):
         co1 = tf.squeeze(tf.einsum('bhwc,bhwd->bhwcd', y_true, rand_comp1), -1)
         co2 = tf.squeeze(tf.einsum('bhwc,bhwd->bhwcd', y_pred, rand_comp2), -1)
 
-        cat_feats1 = tf.concat([y_true, co1], -1)
-        cat_feats2 = tf.concat([y_pred, co2], -1)
-
-        return compute_wass_dist(cat_feats1, cat_feats2, p=2)
+        return compute_wass_dist(co1, co2, p=2)
 
 
 loss_dict = {'m1': FirstMomentLoss(), 'm2': SecondMomentLoss(), 'covar': CovarLoss(), 'gram': GramianLoss(),
