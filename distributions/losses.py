fa0fa51e74b55342fc1b2cddd822dc3adaa72f29
eddie <aigagror@gmail.com>
2021-03-07 23:35:13 -0600

    re added original feats in rand pair wass

--- distributions/losses.py
+++ distributions/losses.py
@@ -98,8 +98,8 @@ class RandPairWassLoss(tf.keras.losses.Loss):
         co1 = tf.squeeze(tf.einsum('bhwc,bhwd->bhwcd', y_true, rand_comp1), -1)
         co2 = tf.squeeze(tf.einsum('bhwc,bhwd->bhwcd', y_pred, rand_comp2), -1)
 
-        cat_feats1 = tf.concat([rand_comp1, co1], axis=-1)
-        cat_feats2 = tf.concat([rand_comp2, co2], axis=-1)
+        cat_feats1 = tf.concat([y_true, co1], axis=-1)
+        cat_feats2 = tf.concat([y_pred, co2], axis=-1)
 
         return compute_wass_dist(cat_feats1, cat_feats2, p=2)
 
