a4cd328c168b104be2b4f2d0a7d2b798d1a3ec99
eddie <aigagror@gmail.com>
2021-03-11 17:20:15 -0600

    temporarily printing shape in wass dist

--- distributions/__init__.py
+++ distributions/__init__.py
@@ -3,8 +3,9 @@ import tensorflow as tf
 
 def compute_wass_dist(y_true, y_pred, p=1):
     shape = tf.shape(y_true)
-    print(shape)
-    bsz, num_locs, channels = shape[0], shape[1] * shape[2], shape[-1]
+    print([tf.shape(y_true)[i] for i in range(4)])
+    print([tf.shape(y_pred)[i] for i in range(4)])
+    bsz, num_locs, channels = shape[0], shape[1] * shape[2], shape[3]
     y_true = tf.reshape(y_true, [bsz, num_locs, channels])
     y_pred = tf.reshape(y_pred, [bsz, num_locs, channels])
     y, x = tf.sort(y_true, axis=1), tf.sort(y_pred, axis=1)
