e12d75ebd34d3e7bb66be0f0e4e1fae5fe207a27
eddie <aigagror@gmail.com>
2021-03-15 14:17:35 -0500

    removed the transfer checkpoint

--- training.py
+++ training.py
@@ -69,7 +69,6 @@ def train(sc_model, ds, out_dir):
     try:
         callbacks = [
             tf.keras.callbacks.CSVLogger(f'{out_dir}/logs.csv'),
-            TransferCheckpoint(out_dir)
         ]
         history = sc_model.fit(ds, epochs=FLAGS.train_steps // FLAGS.steps_exec,
                                steps_per_epoch=FLAGS.steps_exec, verbose=FLAGS.verbose, callbacks=callbacks)
